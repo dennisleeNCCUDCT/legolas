8b5505b19d376d70772b696233f4b340e1a243f1
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2024-01-19 09:10:06 -0800

    update home
    update home

--- src/pages/front/Home.js
+++ src/pages/front/Home.js
@@ -14,9 +14,24 @@ function Home() {return(<div className="container mx-auto">
                 <h2 className="text-2xl font-bold mb-4">嘎拉嘎拉 幫你找園丁</h2>
                 <p className="text-sm mb-4">園丁的照片</p>
                 <button className="bg-green-500 text-white px-4 py-2 text-xs rounded mb-4">照片集</button>
-                <div></div>
-                <img src="https://placehold.co/600x400" alt="Interior design placeholder" className="w-full"/>
-            </div>
+                <div class="home-info relative">
+                <div class="infos absolute inset-0 flex flex-col items-center justify-center z-10">
+        <h3 class="text-xl font-bold">
+         Google 登錄
+        </h3><br></br>
+       
+        <span class=" text-gray px-4 -mt-3 inline-block">或者</span>
+     
+      <input class="rounded-full border border-gray-300 p-2 mt-4 w-240 text-center" placeholder="使用電子信箱註冊" type="text"/>
+      <button class="bg-green-500 text-white px-4 py-2 text-xs rounded mt-4">
+        使用電子信箱註冊
+      </button>
+      <p class="text-xs text-center mt-4 px-6">
+        繼續註冊或登入即表示您同意legolas 使用條款和隱私權政策。我同意 Houzz 可以出於行銷目的向我的電子郵件地址發送電子郵件。您可以隨時透過設定停止它。
+      </p>
+       </div>
+       <img alt="Home renovation placeholder" class="w-full" src="https://placehold.co/600x400"/>
+      </div></div>
             <div className="bg-gray-800 text-white p-6">
                 <h2 className="text-2xl font-bold mb-4">超強園丁搓合網站</h2>
                 <p className="text-sm mb-4">為園丁專家提供支援的專業平台。</p>
