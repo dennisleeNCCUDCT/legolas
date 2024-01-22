8b7bbd25829bfc56247aeb1491d14e0bf6c0fafc
dennisleeNCCUDCT <95065965+dennisleeNCCUDCT@users.noreply.github.com>
2024-01-22 10:26:53 -0800

    駝峰2 fixed
    駝峰2 fixed

---
+++ src/pages/front/PersonalHomepage.js
@@ -0,0 +1,65 @@
+function PersonalHomepage(){return(
+    <body class="bg-gray-100">
+  <div class="container mx-auto px-4">
+   <div class="grid grid-cols-3 gap-4 py-8">
+
+    <div class="col-span-2">
+     <img alt="Spacious hallway with shoe storage, wooden bench and a wooden door at the end" class="w-full h-auto rounded" height="400" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-ineTRGliVEVgYWpuWMxLSJPl/user-AeGcMTsJCMHY0HHXnkfYEfKu/img-jh4efdmYH6NDLra1vPIjfYRG.png?st=2024-01-22T01%3A03%3A13Z&amp;se=2024-01-22T03%3A03%3A13Z&amp;sp=r&amp;sv=2021-08-06&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-01-21T19%3A42%3A29Z&amp;ske=2024-01-22T19%3A42%3A29Z&amp;sks=b&amp;skv=2021-08-06&amp;sig=otNbNNYADFuOXoEZrZB6IQ9hM%2BNVP6GkMHPUfIIoSx8%3D" width="600"/>
+     <p class="mt-2 text-sm">
+      すっきりと収納を実現する、シューズクロークをつくろう
+     </p>
+    </div>
+
+    <div class="space-y-4">
+     <div>
+      <img alt="Cozy living room with modern furniture and large windows" class="w-full h-auto rounded" height="200" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-ineTRGliVEVgYWpuWMxLSJPl/user-AeGcMTsJCMHY0HHXnkfYEfKu/img-TdQlAL4BYenkIn4kc4NlMPEg.png?st=2024-01-22T01%3A03%3A14Z&amp;se=2024-01-22T03%3A03%3A14Z&amp;sp=r&amp;sv=2021-08-06&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-01-21T19%3A56%3A18Z&amp;ske=2024-01-22T19%3A56%3A18Z&amp;sks=b&amp;skv=2021-08-06&amp;sig=eh1urb5YZg2mvcsJuZwMjzSL5zuR3F2cd3GY%2Bm9rXaM%3D" width="300"/>
+      <p class="mt-2 text-xs">
+       リビング
+      </p>
+     </div>
+     <div>
+      <img alt="Elegant wooden deck with two chairs and a table, surrounded by trees" class="w-full h-auto rounded" height="200" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-ineTRGliVEVgYWpuWMxLSJPl/user-AeGcMTsJCMHY0HHXnkfYEfKu/img-rB1wD4d7dR8yq30dfctVfkuN.png?st=2024-01-22T01%3A03%3A14Z&amp;se=2024-01-22T03%3A03%3A14Z&amp;sp=r&amp;sv=2021-08-06&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-01-21T19%3A42%3A51Z&amp;ske=2024-01-22T19%3A42%3A51Z&amp;sks=b&amp;skv=2021-08-06&amp;sig=tFVHcvK1yuAemWmczC%2BsZp5N8R4OOwkQ4pLwpBHzwA4%3D" width="300"/>
+      <p class="mt-2 text-xs">
+       エクステリアの人気記事
+      </p>
+     </div>
+     <div>
+      <img alt="Modern kitchen with white cabinets, wooden countertops and brass fixtures" class="w-full h-auto rounded" src="https://placehold.co/300x200"/>
+      <p class="mt-2 text-xs">
+       [リノベ] S PROJECT, TOKYO
+      </p>
+     </div>
+    </div>
+   </div>
+
+   <div class="grid grid-cols-4 gap-4 pb-8">
+    <div class="col-span-1">
+     <img alt="Modern dining area with a wooden table, chairs and a pendant light" class="w-full h-auto rounded" src="https://placehold.co/300x200"/>
+     <p class="mt-2 text-xs">
+      人気のダイニングギャラリーの写真
+     </p>
+    </div>
+    <div class="col-span-1">
+     <img alt="Minimalist white house with large windows and a tree in front" class="w-full h-auto rounded" src="https://placehold.co/300x200"/>
+     <p class="mt-2 text-xs">
+      家づくりのヒント&amp;おすすめ記事
+     </p>
+    </div>
+    <div class="col-span-1">
+     <img alt="Luxurious kitchen with white cabinetry, gold fixtures and a gray island" class="w-full h-auto rounded" height="200" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-ineTRGliVEVgYWpuWMxLSJPl/user-AeGcMTsJCMHY0HHXnkfYEfKu/img-msRYOGVdNV161HEKI0O881aR.png?st=2024-01-22T01%3A03%3A14Z&amp;se=2024-01-22T03%3A03%3A14Z&amp;sp=r&amp;sv=2021-08-06&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-01-21T14%3A18%3A07Z&amp;ske=2024-01-22T14%3A18%3A07Z&amp;sks=b&amp;skv=2021-08-06&amp;sig=O2JuFcwFH642%2Bq594vALsXv1jHbzrWwfgJfFuywW6uE%3D" width="300"/>
+     <p class="mt-2 text-xs">
+      [リノベ] S PROJECT, TOKYO
+     </p>
+    </div>
+    <div class="col-span-1">
+     <img alt="Close-up of a floral arrangement with dried flowers and leaves" class="w-full h-auto rounded" height="200" src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-ineTRGliVEVgYWpuWMxLSJPl/user-AeGcMTsJCMHY0HHXnkfYEfKu/img-hDLWNxIF8aLyLkG7HskBfdBy.png?st=2024-01-22T01%3A03%3A14Z&amp;se=2024-01-22T03%3A03%3A14Z&amp;sp=r&amp;sv=2021-08-06&amp;sr=b&amp;rscd=inline&amp;rsct=image/png&amp;skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&amp;sktid=a48cca56-e6da-484e-a814-9c849652bcb3&amp;skt=2024-01-21T19%3A46%3A18Z&amp;ske=2024-01-22T19%3A46%3A18Z&amp;sks=b&amp;skv=2021-08-06&amp;sig=MPSeF8xOhZJRyjzJsVDYF31t11XXvNrhzBgxUgY9BXs%3D" width="300"/>
+     <p class="mt-2 text-xs">
+      花とともに暮らす
+     </p>
+    </div>
+   </div>
+  </div>
+ </body>
+)}
+
+export default PersonalHomepage
